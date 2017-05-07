8baedbd87e3fbbfd5621f738bddfdd870d52c8ad
Hiroyuki Miyazaki <miyazaki@mcommit.com>
2017-05-07 18:38:44 +0900

    ログ出力処理を修正

--- app_logger.rb
+++ app_logger.rb
@@ -6,14 +6,32 @@ class AppLoger
   LOG_LEVEL_ERROR = "error"
 
   class << self
-    def setup descripter=STDOUT, level=nil
-      @@log = Logger.new(descripter)
+    def setup progname=nil, descripter=STDOUT, level=LOG_LEVEL_DEBUG
+      @@logger = Logger.new(descripter)
+      @@logger.datetime_format = '%Y-%m-%d %H:%M:%s'
+      @@logger.formatter =
+        proc { |severity, datetime, progname, msg| "[#{datetime}] [#{severity}] #{msg}\n"}
     end
 
     def trace(msg, level=LOG_LEVEL_DEBUG)
       caller_info = caller.first
-      line = caller_info + msg
-      @@log.send(level, line)
+      filename = File.basename(caller_info)
+      line = filename + " " + msg
+      @@logger.send(level, line)
+    end
+
+    def call_in
+      caller_info = caller.first
+      filename = File.basename(caller_info)
+      line = filename + " " + "Called In..."
+      @@logger.send(LOG_LEVEL_DEBUG, line)
+    end
+
+    def call_out
+      caller_info = caller.first
+      filename = File.basename(caller_info)
+      line = filename + " " + "Called Out..."
+      @@logger.send(LOG_LEVEL_DEBUG, line)
     end
   end
 end
