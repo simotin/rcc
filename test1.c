5c1e129542124ddcc46ecb85e3efbde789473250
Hiroyuki Miyazaki <miyazaki@mcommit.com>
2017-06-20 16:46:52 +0900

    実装中なので簡易なコードに修正

--- test1.c
+++ test1.c
@@ -1,38 +1,11 @@
-// テストコード
-// 注意 			: テストコードは常にgccでコンパイルできることを確認する gcc -c test.c
-// - 変数宣言
-// - 代入
-// - 関数宣言
-// - 関数呼び出し
-// // のコメントアウト
-// をサポートする
-// hoge関数は引数を標準出力する組み込み関数とする
-// 文字コードはUTF-8,改行コードはLFのみとする
-int number = 10;
-int main(int argc, char **argv)
+void fff(int a, int b)
 {
-	int a;
-	{
-		{
-		}
-	}
-	// //コメントはコメントアウト
-	a = 123;				// 末尾のコメントもプリプロセッサによって削除
-	hoge(a);
-	return 0;
+	return 3;
 }
 
-void fff(int fff)
+int main(int argc, char **argv)
 {
-	/* comment あ */
-	#if 1
-	if (a == 222) {
-		switch (b == 333)	// fff
-		{
-		case 1:
-		}
-	}else {
-	}
-	#endif
-	return;
+	int ret;
+	fff(123, 456);
+	return 0;
 }
